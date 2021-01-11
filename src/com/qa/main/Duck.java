0150e08f07597d569337c5ae64b777b521ae6df7
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-01-11 17:14:54 +0000

    Duck Test QA project

---
+++ src/com/qa/main/Duck.java
@@ -0,0 +1,88 @@
+package com.qa.main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Duck {
+
+    private String name;
+    private boolean isAMallard;
+    private List<Duck> myDuckList = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAMallard() {
+        return isAMallard;
+    }
+
+    public void setAMallard(boolean AMallard) {
+        this.isAMallard = AMallard;
+    }
+
+    public List<Duck> getMyDuckList() {
+        return myDuckList;
+    }
+
+    public void setMyDuckList(List<Duck> myDuckList) {
+        this.myDuckList = myDuckList;
+    }
+
+    public void addToDuckList(Duck duck) {
+        this.myDuckList.add(duck);
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" +
+                "name='" + name + '\'' +
+                ", isAMallard=" + isAMallard +
+                '}';
+    }
+
+    public void printAll() {
+        for (Duck duck : this.myDuckList) {
+            System.out.println(duck.toString());
+        }
+    }
+
+    public void printAllList() {
+        System.out.println(this.myDuckList);
+    }
+
+    public void printAllMallard() {
+        for (Duck duck : this.myDuckList) {
+            if (duck.isAMallard()) {
+                System.out.println(duck);
+            }
+        }
+    }
+
+    public void sortAllDucks() {
+        List<String> nameList = new ArrayList<>();
+        for (Duck duck : this.myDuckList) {
+            nameList.add(duck.name);
+        }
+        Collections.sort(nameList);
+
+        List<Duck> myDuckList2 = new ArrayList<>();
+
+        for (String name : nameList) {
+            for (Duck duck : this.myDuckList) {
+                if (duck.name.equals(name)) {
+                    myDuckList2.add(duck);
+                }
+            }
+        }
+
+        myDuckList = myDuckList2;
+
+    }
+
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,44 @@
+package com.qa.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Duck woodDuck = new Duck();
+        woodDuck.setName("Wood Duck");
+        woodDuck.setAMallard(false);
+
+        Duck mallard = new Duck();
+        mallard.setName("Mallard Duck");
+        mallard.setAMallard(true);
+
+        Duck redheadDuck = new Duck();
+        redheadDuck.setName("Redhead Duck");
+        redheadDuck.setAMallard(false);
+
+        Duck myDuckList =  new Duck();
+        myDuckList.addToDuckList(woodDuck);
+        myDuckList.addToDuckList(mallard);
+        myDuckList.addToDuckList(redheadDuck);
+
+        System.out.println("Printing all ducks in list");
+        myDuckList.printAllList();
+        System.out.println("\n");
+
+        System.out.println("Printing all ducks individually");
+        myDuckList.printAll();
+        System.out.println("\n");
+
+        System.out.println("Printing all mallards");
+        myDuckList.printAllMallard();
+
+        System.out.println("Sorting List");
+        myDuckList.printAllList();
+        myDuckList.sortAllDucks();
+        myDuckList.printAllList();
+
+    }
+}
